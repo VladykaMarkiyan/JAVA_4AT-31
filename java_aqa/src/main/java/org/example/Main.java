ea085ce07d95f6d78b59e6304bbe331744c3562a
VladykaMarkiyan <144060935+VladykaMarkiyan@users.noreply.github.com>
2025-09-05 17:04:51 +0300

    Update Main.java

--- java_aqa/src/main/java/org/example/Main.java
+++ java_aqa/src/main/java/org/example/Main.java
@@ -1,4 +1,10 @@
 package org.example;
+// V 4: Primitive Arrays 
+// Objective: To understand and use arrays in Java with primitive types.
+// Instructions:
+// Declare and initialize an array of integers.
+// Use a for loop to print the elements of the array to the console.
+// Use a for loop to find the sum of the elements in the array.
 
 public class Main {
     public static void main(String[] args) {
